e13d9abdceb5663eaebbb78d8a3cd60c24a181a9
sewaustav <sewabobrov04@gmail.com>
2025-11-13 17:25:55 +0100

    hyprlock

--- hypr/scripts/kitty.sh
+++ hypr/scripts/kitty.sh
@@ -5,7 +5,7 @@ app=$(hyprctl activewindow -j | jq -r '.class')
 hyprctl dispatch togglefloating
 
 if [[ "$app" == "kitty" ]]; then
-    hyprctl dispatch resizeactive exact 650 550
+    hyprctl dispatch resizeactive exact 800 520
 else
     hyprctl dispatch resizeactive exact 800 600
 fi
