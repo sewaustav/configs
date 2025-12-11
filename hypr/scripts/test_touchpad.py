78a9af613270e7bb92dc94e9fc38b6d33877e0a8
sewaustav <sewabobrov04@gmail.com>
2025-12-12 00:39:18 +0100

    touchpad settings

--- hypr/scripts/test_touchpad.py
+++
@@ -1,9 +0,0 @@
-import pyudev
-
-context = pyudev.Context()
-monitor = pyudev.Monitor.from_netlink(context)
-monitor.filter_by(subsystem="input")
-
-for device in iter(monitor.poll, None):
-    if device.action in ("add", "remove"):
-        print(device.action, device)
--- hypr/scripts/touchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true; do
-    if hyprctl devices | grep -q "Mouse.*USB"; then
-        echo "мышь подключена"
-    else
-        echo "мышь не подключена"
-    fi
-    sleep 5
-done
