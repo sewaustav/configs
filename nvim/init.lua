40deea66fb7751366b0da8098d9140c16a4b45e9
sewaustav <sewabobrov04@gmail.com>
2026-08-27 17:20:05 +0200

    noctalia -> dms

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,2 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.clipboard = "unnamedplus"
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
